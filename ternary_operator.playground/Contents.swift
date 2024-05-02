7282983917c658da783f3b255158da7a800857b2
Gyunorld <mingue640@gmail.com>
2024-05-02 21:21:50 +0900

    삼항연산자

---
+++ ternary_operator.playground/Contents.swift
@@ -0,0 +1,30 @@
+// 삼항연산자
+let num1 = 10
+let num2 = -10
+
+let result = (num2 > 0) ? "양수다" : "양수가 아니다"
+
+// chap02_practice_01
+let a = 9
+let b = a % 2
+
+var result1 = (b == 1) ? "홀수" : "짝수"
+
+// chap02_practice_02
+var age1 = 13
+var age2 = 16
+var age3 = 19
+
+let result2 = (age1 <= 13) ? "어린이" : (age1 < 19) ? "청소년" : "성인"
+let result3 = (age2 <= 13) ? "어린이" : (age2 < 19) ? "청소년" : "성인"
+let result4 = (age3 <= 13) ? "어린이" : (age3 < 19) ? "청소년" : "성인"
+
+// chap02_practice_03
+var gender1 = "M"
+
+let result5  = (gender1 == "M") ? "남자입니다" : (gender1 == "F") ? "여자입니다" : "잘못입력하셨습니다"
+
+var gender2 = "F"
+
+let result6 = (gender2 == "M") ? "남자입니다" : (gender2 == "F") ? "여자입니다" : "잘못입력하셨습니다"
+
